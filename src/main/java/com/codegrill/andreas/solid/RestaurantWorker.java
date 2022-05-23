ab8d40e9965bb6919a3b1d25638f219534d600d8
mouzong <andreasmouzong@gmail.com>
2022-05-23 13:30:17 +0100

    added raw non MVC restaurantWorker class

---
+++ src/main/java/com/codegrill/andreas/solid/RestaurantWorker.java
@@ -0,0 +1,10 @@
+package com.codegrill.andreas.solid;
+
+class RestaurantWorker {
+    public void seatPatrons();
+    public void takeOrder();
+    public void cookOrder();
+    public void deliverOrder();
+    public void cleanTable();
+    public void washDishes();
+}
